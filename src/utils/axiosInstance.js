556a73b00d40a29405d2dec4f0a7ff525c57d53c
shekhararpitji <shekhararpitji@gmail.com>
2025-05-28 10:39:53 +0530

    Fixed toast, login page

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 6000,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
